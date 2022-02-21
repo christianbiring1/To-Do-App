4849ece186f1be8c3f0d980c3ad83c57c9bf90d2
christianbiring1 <christianbiringanine22@gmail.com>
2022-02-21 13:56:58 +0200

    Add does on the list

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-const item = document.querySelector('input[type="text"]');
+const container = document.querySelector('.activities');
+const formElement = document.forms['manipulate-item'];
+formElement.addEventListener('submit', e =>{
+    e.preventDefault();
+    
+})
+     /* Add item on the list */
+const link = formElement.querySelector('.submit');
+link.addEventListener('click', e => {
+
+        // Grab typed item.
+        const item = formElement.querySelector('input[type="text"]').value;
+        console.log(item);
+
+            //  create Element
+    const ul = document.createElement('ul');
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+
+    //  Append element
+    container.appendChild(ul);
+    ul.appendChild(li);
+    li.appendChild(span);
+
+    // ADD content
+    span.textContent = item;
+    
+})
+
+
+
+
+
+
+
+
+
+
+
